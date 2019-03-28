54283b3e74870b7b8e32b5a06eb162e4114556ca
hileix <304192604@qq.com>
2019-03-28 16:01:36 +0800

    update

--- components/DashboardPage/stories/DashboardPage.stories.js
+++ components/DashboardPage/stories/DashboardPage.stories.js
@@ -1,15 +1,13 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
-import DashboardPage from '../DashboardPage';
+import DashboardPageWrap from './DashboardPageWrap';
 import './index.css';
 import '../style/index.less';
 
-import { rows } from './mockData';
 const baseURL = 'http://kingofdinner.realsun.me:8102/';
 
 storiesOf('DashboardPage 仪表盘页面', module).add('DashboardPage', () => (
   <div>
-    <DashboardPage mode="view" rows={rows} baseURL={baseURL} />
-    dashboard
+    <DashboardPageWrap />
   </div>
 ));
---
+++ components/DashboardPage/stories/DashboardPageWrap.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import DashboardPage from '../DashboardPage';
+import './index.css';
+import '../style/index.less';
+import http from 'lz-request/lib/http';
+
+/**
+ * 获取某个报表分类的所有仪表盘
+ * @param {number} UniID 分类 id
+ */
+http.createApi('getDashboards', {
+  url: '/api/Reports/GetUserReports'
+});
+
+http.setDefaultBaseURL('http://kingofdinner.realsun.me:8102');
+
+export default class DashboardPageWrap extends React.Component {
+  static propTypes = {};
+
+  state = {
+    dashboardList: []
+  };
+
+  componentDidMount = () => {
+    this.getData();
+  };
+
+  getData = async () => {
+    let res;
+    try {
+      res = await http().getDashboards({ UniID: 605371353285 });
+    } catch (err) {
+      return console.error(err);
+    }
+    console.log({ resData: res.data });
+    this.setState({ dashboardList: res.data });
+  };
+
+  render() {
+    return (
+      <div>
+        <DashboardPage rows={[]} mode="view" />
+      </div>
+    );
+  }
+}
