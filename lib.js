ff62602b571200288aa753c07050b68cb1eaa725
hileix <304192604@qq.com>
2019-03-28 10:28:47 +0800

    add .npmignore file

--- lib.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-
-var _react = _interopRequireDefault(require("react"));
-
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
-
-var a = _react.default.createElement("div", null, "a");
